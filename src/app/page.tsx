8df362d967d6629c46b10b5fe455422712264013
ssarker <ssarker@vantage.com>
2026-07-06 10:05:22 +0600

    Restore hero image rotation: add 3 new knockout-stage hero images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,9 +75,9 @@ export default async function Home() {
         <Image
           src={(() => {
             const heroes = [
-              "/heroes/legends-1.png",
               "/heroes/legends-2.png",
               "/heroes/legends-3.png",
+              "/heroes/legends-4.png",
               "/heroes/legends-5.png"
             ];
             return heroes[Math.floor(Math.random() * heroes.length)];
